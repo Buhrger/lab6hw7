4ef48743dc6adb77cf8cb592b540f528425bcc29
oabdullahi8 <33332538+oabdullahi8@users.noreply.github.com>
2018-10-25 19:14:39 -0400

    Update RemoteFileBrowser.java

--- RemoteFileBrowser.java
+++ RemoteFileBrowser.java
@@ -1,192 +1,190 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import javafx.application.*;
+import javafx.event.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.*;
+import javafx.scene.text.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+import javafx.geometry.*;
+
 import java.net.*;
 import java.io.*;
 import java.util.*;
-import javax.swing.text.*;
-import javax.swing.filechooser.*;
-import java.util.*;
 
-/* 
- * File server
- *
- * @authors Josh Burger, Dustin Rochette
- * @version 3/7/18
- */
- 
-public class RemoteFileBrowser extends JFrame implements ActionListener
-{
-   // Attributes declared here
-   JLabel jlName = new JLabel("Server:");
-   JTextField jtfName = new JTextField(20);
-   JButton jbConnect = new JButton("Connect");
+/*
+*
+*@author: Omar Abdullahi , Will, Burger, Dustin
+*@Version: 10/25/2018
+*@Course: ISTE-121
+*@Description: RemoteFileBrowser
+*
+*/
+
+public class RemoteFileBrowser extends Application implements EventHandler<ActionEvent> {
+   // Window Attributes
+   private Stage stage;
+   private Scene scene;
+   private VBox root;
    
-   JButton jbList = new JButton("List");
-   JButton jbCD = new JButton("Ch Dir");
-   JButton jbUpload = new JButton("Upload");
-   JButton jbDownload = new JButton("Download");
+   // Components - TOP
+   // The TOP will itself be GridPane ... we will use Row1 and Row2 of the this Grid
+   // These are for Row1
+   private Label lblServerIP = new Label("Server:");
+   private TextField tfServer = new TextField();
+   private Button btnConnect = new Button("Connect");
+
+   // These will be in Row2
+   private Button btnList = new Button("List");
+   private Button btnCD = new Button("Ch Dir");
+   private Button btnUpload = new Button("Upload");
+   private Button btnDownload = new Button("Download");
    
-   JLabel jlLog = new JLabel("Log:");
-   JTextArea jtaLog = new JTextArea(10, 35);
-   
-   JPanel jpNorth = new JPanel();
-   JPanel jpCenter = new JPanel();
-   
+   //These will be in Row3 for Logs
+   private TextArea taLog = new TextArea();
+
+   // IO attributes
+   private PrintWriter pwt = null;
+   private Scanner scn = null;
+
+   // OTHER attributes
    public static final int SERVER_PORT = 32001;
    private Socket socket = null;
-   private FileOutputStream fos = null;
-   private FileInputStream fis = null;
+
+   /**
+    * main program 
+    */
+   public static void main(String[] args) {
+      launch(args);
+   }
+
+   /**
+    * Constructor ... draw and set up GUI
+    */
+   public void start(Stage _stage) {
+      stage = _stage;
+      stage.setTitle("RemoteFile Browser");
+      stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+         public void handle(WindowEvent evt) { System.exit(0); }
+      } );
+      stage.setResizable(false);
+      root = new VBox(8);
+      
+      // ROW1 ... FlowPane ... 
+      FlowPane fpRow1 = new FlowPane(8,8);
+         fpRow1.setAlignment(Pos.CENTER);
+         tfServer.setPrefColumnCount(15);
+         fpRow1.getChildren().addAll(lblServerIP,tfServer, btnConnect);
+      root.getChildren().add(fpRow1);
+
+      // ROW2 - Buttons for list/change Dir/Upload/ and Download
+      FlowPane fpRow2 = new FlowPane(8,8);
+         fpRow2.setAlignment(Pos.CENTER);
+         fpRow2.getChildren().addAll( btnList, btnCD, btnUpload, btnDownload);
+         
+         // ROW2- buttons disabled until connected
+         btnList.setDisable(true);
+         btnCD.setDisable(true);
+         btnUpload.setDisable(true);
+         btnDownload.setDisable(true);
+         root.getChildren().add(fpRow2);
+      
+      // LOG ... Label + text area
+      FlowPane fpLog = new FlowPane();
+         fpLog.setAlignment(Pos.CENTER);
+         taLog.setPrefColumnCount(35);
+         taLog.setPrefRowCount(10);
+         fpLog.getChildren().addAll(taLog);
+      root.getChildren().add(fpLog);
+      
+      // Listen for the buttons
+      btnConnect.setOnAction(this);
+
+      scene = new Scene(root, 475, 300);
+      stage.setScene(scene);
+      stage.show();      
+   }
    
-   // Main program
-   public static void main(String [] args)
-   {
-      new RemoteFileBrowser();
-   }
-      // Constructor
-   public RemoteFileBrowser()
-   {
-      setupWindow();
-      jpNorth.setLayout(new GridLayout(0, 1));
-      
-      JPanel jpRow1 = new JPanel();
-         jpRow1.setLayout(new FlowLayout(FlowLayout.CENTER));
-         jpRow1.add(jlName);
-         jpRow1.add(jtfName);
-         jpRow1.add(jbConnect);
-         jpNorth.add(jpRow1);
-         
-      JPanel jpRow2 = new JPanel();
-         jpRow2.setLayout(new FlowLayout(FlowLayout.CENTER));
-         jpRow2.add(jbList);
-         jpRow2.add(jbCD);
-         jpRow2.add(jbUpload);
-         jpRow2.add(jbDownload);
-         jpNorth.add(jpRow2);
-      this.add(jpNorth, BorderLayout.NORTH);
-      
-      DefaultCaret caret = (DefaultCaret)jtaLog.getCaret();
-      caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
-      jtaLog.setLineWrap(true);
-      jtaLog.setWrapStyleWord(true);
-      JScrollPane jsp = new JScrollPane(jtaLog);
-      this.add(jsp);
-         
-      jpCenter.add(jlLog);
-      jpCenter.add(jtaLog);
-      this.add(jpCenter, BorderLayout.CENTER);
-         
-      jbConnect.addActionListener(this);
-      jbList.addActionListener(this);
-      jbCD.addActionListener(this);
-      jbUpload.addActionListener(this);
-      jbDownload.addActionListener(this);     
-      jtaLog.setEditable(false);
-      
-      
-      //jbList.setEnabled(false);
-      //jbCD.setEnabled(false);
-      //jbUpload.setEnabled(false);
-      //jbDownload.setEnabled(false);
-         
-      this.setVisible(true);
-   }
-      
-   public void setupWindow()
-   {
-      this.setTitle("Remote File Browser (J. Burger, C. McManus)");
-      this.setSize(600, 325);
-      this.setLocation(100, 100);
-      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-   }
-         
-   public void actionPerformed(ActionEvent ae) {
-      switch(ae.getActionCommand()) {
+
+ /** 
+    * Button dispatcher
+    */
+       
+    public void handle(ActionEvent ae) {
+    /*
+      String label = ((Button)ae.getSource()).getText();
+      switch(label) {
          case "Connect":
             doConnect();
             break;
          case "Disconnect":
             doDisconnect();
             break;
-         case "List":
-            doList();
-            break;
-         case "Ch Dir":
-            doCD();
-            break;
-         case "Upload":
-            doUpload();
-            break;
-         case "Download":
-            doDownload();
+         case "Send":
+            doSend();
             break;
       }
+      */
    }
-   
-   private void doConnect()
-   {
+
+   /**
+    * doConnect - Connect button
+    */
+       /*
+   private void doConnect() {
       try {
-         socket = new Socket(jtfName.getText(), SERVER_PORT);
-               }
+         // Connect to server and set up two streams, a Scanner for input from the
+         // server and a PrintWriter for output to the server
+         socket = new Socket(tfServerIP.getText(), SERVER_PORT);
+         scn = new Scanner(new InputStreamReader(socket.getInputStream()));
+         pwt = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+      }
       catch(IOException ioe) {
-         jtaLog.append("IO Exception: " + ioe + "\n");
+         taLog.appendText("IO Exception: " + ioe + "\n");
          return;
       }
-      jtaLog.append("Connected!\n");
-      jbConnect.setText("Disconnect");
-      jbList.setEnabled(true);
-      jbCD.setEnabled(true);
-      jbUpload.setEnabled(true);
-      jbDownload.setEnabled(true);
+      taLog.appendText("Connected!\n");
+      btnConnect.setText("Disconnect");
       
-      
+      // Enable text field and Send button
+      tfSentence.setDisable(false);
+      btnSend.setDisable(false);
    }
-   
-   private void doDisconnect()
-   {
+
+   /**
+    * doDisconnect - Disconnect button'
+    */
+       /*
+   private void doDisconnect() {
       try {
+         // Close the socket and streams
          socket.close();
+         scn.close();
+         pwt.close();
       }
       catch(IOException ioe) {
-         jtaLog.append("IO Exception: " + ioe + "\n");
+         taLog.appendText("IO Exception: " + ioe + "\n");
          return;
       }
-      jbConnect.setText("Connect");
-      jbList.setEnabled(false);
-      jbCD.setEnabled(false);
-      jbUpload.setEnabled(false);
-      jbDownload.setEnabled(false);
-         }
-         
-      public void doUpload()
-      {
-         JOptionPane.showInputDialog(null, 
-         "Input Filename: ", "File Upload", 
-         JOptionPane.QUESTION_MESSAGE);
+      btnConnect.setText("Connect");
+      
+      // Disable text field and Send button
+      tfSentence.setDisable(true);
+      btnSend.setDisable(true);
+   }
 
-      }
-      
-      public void doDownload()
-      {
-         JOptionPane.showInputDialog(null, 
-         "Input Filename: ", "File Download", 
-         JOptionPane.QUESTION_MESSAGE);
-      }
-      
-      public void doList()
-      {
-         File workingDirectory = new File(System.getProperty("user.dir"));
-         jtaLog.append(Arrays.toString(workingDirectory.list()));
-      }
-      
-      public void doCD()
-      {
-         JOptionPane.showInputDialog(null, 
-         "Input New Directory: ", "Change Directory", 
-         JOptionPane.QUESTION_MESSAGE);
-      }
-   
- 
-
-   
+   /**
+    * doSend - Send button'
+    */
+   /*    
+   private void doSend() {
+      // Get the sentence, send to server, wait for reply
+      pwt.println(tfSentence.getText());
+      pwt.flush();
+      taLog.appendText("Sent: " + tfSentence.getText() + "\n");
+      tfSentence.setText("");
+      String reply = scn.nextLine();
+      taLog.appendText("Reply: " + reply + "\n");
+   }
+ */
 }
