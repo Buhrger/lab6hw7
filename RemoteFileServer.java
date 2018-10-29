7e67d422e07db33e66e70349d3b6cc0f4f34ed74
oabdullahi8 <33332538+oabdullahi8@users.noreply.github.com>
2018-10-28 20:29:13 -0400

    Update RemoteFileServer.java

--- RemoteFileServer.java
+++ RemoteFileServer.java
@@ -1,170 +1,114 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
 import java.net.*;
 import java.io.*;
 import java.util.*;
-import javax.swing.text.*;
+import javafx.application.*;
+import javafx.event.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+import javafx.geometry.*;
 
+/*
+*
+*@author: Omar Abdullahi , Will, Burger, Dustin
+*@Version: 10/25/2018
+*@Course: ISTE-121
+*@Description: RemoteFileServer
+*
+*/
+public class RemoteFileServer extends Application implements EventHandler<ActionEvent> {
+   // Window Attributes
+	private Stage stage;
+	private Scene scene;
+	private VBox root = new VBox(8);
 
-public class RemoteFileServer extends JFrame implements ActionListener {
-   // GUI Components
-   public JLabel jlLog = new JLabel("Log:");
-   public JTextArea jtaLog = new JTextArea(10, 35);
-   private JButton jbStart = new JButton("Start");
+	// GUI Components
+	private Button btnStart = new Button("Start");
+	private TextArea taLog = new TextArea();
+
+	// Socket, IO
+	private ServerSocket sSocket = null;
+	public static final int SERVER_PORT = 32001;
+	private PrintWriter pwt = null;
+	private Scanner scn = null;
+
+	// Main
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	// Start
+	public void start(Stage _stage) {
+		// Window
+		stage = _stage;
+		stage.setTitle("Remote File Server");
+
+		// Button Set On Action
+		btnStart.setOnAction(this);
+
+		// Top GUI
+		FlowPane fpTop = new FlowPane(8, 8);
+		fpTop.getChildren().addAll(btnStart);
+		fpTop.setAlignment(Pos.TOP_RIGHT);
+		root.getChildren().addAll(fpTop);
+
+		// Bottom GUI
+		FlowPane fpBottom = new FlowPane(8, 8);
+		fpBottom.getChildren().addAll(taLog);
+		taLog.setWrapText(true);
+		root.getChildren().addAll(fpBottom);
+
+		// Show window
+		scene = new Scene(root, 475, 250);
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	// Button Handlers
+	public void handle(ActionEvent javaGunners) {
+		/*String label = ((Button)evt.getSource()).getText();
+		switch(label) {
+			case "Start":
+				// Start ConnectionThread to open socket
+				doStart();
+				break;
+			case "Stop":
+				// Close socket
+				doStop();
+				break;
+		}
+      */
+	}
+
+	// Log
+   /*
+	public void log(String message) {
+		Platform.runLater(new Runnable() {
+			public void run() {
+				taLog.appendText("<localhost: " + SERVER_PORT + "> " + message + "\n");
+			}
+		});
+	}
    
-   // Socket stuff
-   private ServerSocket sSocket = null;
-   public static final int SERVER_PORT = 32001;
    
-   private Thread sThread = null;
-   private boolean running = true;
-   
-   /**
-    * main program
-    */
-   public static void main(String[] args) {
-      new RemoteFileServer();
-   }
-   
-   /**
-    * Constructor, draw and set up GUI
-    * Do server stuff
-    */
-   public RemoteFileServer() {
-      // Window setup
-      this.setTitle("Remote File Server (J. Burger)");
-      this.setSize(600, 300);
-      this.setLocation(800, 100);
-      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-   
-      DefaultCaret caret = (DefaultCaret)jtaLog.getCaret();
-      caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
-      jtaLog.setLineWrap(true);
-      jtaLog.setWrapStyleWord(true);
-      JScrollPane jsp = new JScrollPane(jtaLog);
-      this.add(jsp);
-      
-      // CENTER components
-      this.add(jlLog);
-      this.add(new JScrollPane(jtaLog));
-      
-      // NORTH COMPONENTS
-      JPanel jpNorth = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-      jpNorth.add(jbStart);
-      this.add(jpNorth, BorderLayout.NORTH);
-      jbStart.addActionListener(this);
-      
-      this.setVisible(true);
-      //doServerWork();
-   }
-   
-   public void actionPerformed(ActionEvent ae)
-   {
-      switch(ae.getActionCommand())
-      {
-         case "Start":
-            doStart();
+     //  public void handle(ActionEvent ae) {
+    /*
+      String label = ((Button)ae.getSource()).getText();
+      switch(label) {
+         case "Connect":
+            doConnect();
             break;
-         case "Stop":
-            doStop();
+         case "Disconnect":
+            doDisconnect();
+            break;
+         case "Send":
+            doSend();
             break;
       }
-   }
-   
-   public void doStart()
-   {
-      sThread = new ServerThread();
-      sThread.start();
-      running = true;
-      jbStart.setText("Stop");
-      jtaLog.append("Server Running...\n");
-   }
-   
-   public void doStop()
-   {
-      jbStart.setText("Start");
-      //sThread.kill();
-      jtaLog.append("Server Stopped...\n");
-   }
-   
-   /** ServerThread - does the basic non-GUI work of the server */
-   class ServerThread extends Thread {
-      public void run()
-      {
-         // Claim the port and start listening for new connections
-         try {
-            sSocket = new ServerSocket(SERVER_PORT);
-         }
-         catch(IOException ioe) {
-            jtaLog.append("IO Exception (1): "+ ioe);
-            return;
-         }
-      
-         while(running){
-            Socket cSocket = null;
-            //label = cSocket.getInetAddress().getHostAddress() + ":" + cSocket.getPort() + "::";
-            try {
-               cSocket = sSocket.accept();
-            }
-            catch(IOException ioe) {
-               jtaLog.append("IO Exception (2): "+ ioe);
-               return;
-            }
-         
-            jtaLog.append("Client connected!\n");
-            try{
-                  ClientThread ct = new ClientThread(cSocket);
-                  ct.start();
-               }
-            catch(Exception e) {return;}
-         } 
-      }
-      
-      
-   }//End of ServerThread
-    
-   class ClientThread extends Thread 
-   {
-      private Socket cSocket;
-      
-      public ClientThread(Socket _cSocket)
-      {
-         cSocket = _cSocket;
-      }
-      
-      public void run()
-      {
-         try{
-            Scanner scn = new Scanner(new InputStreamReader(cSocket.getInputStream()));
-            PrintWriter pwt = new PrintWriter(new OutputStreamWriter(cSocket.getOutputStream()));
-                
-            while(true) {
-               String sentence = scn.nextLine();
-               synchronized(jtaLog){
-                  jtaLog.append("Sentence: " + sentence + "\n");
-               }
-               String reply = sentence.toUpperCase();
-               pwt.println(reply);
-               pwt.flush(); 
-               synchronized(jtaLog)
-               {
-                  jtaLog.append("Reply: " + reply + "\n");
-               }
-            }
-         }
-         catch(Exception e) {}
-      } 
-         public void kill()
-      {
-         running = false;
-         try {
-            sSocket.close();
-         }
-         catch(Exception e) {}
-      }
-      
-   }//End of ClientThread
+      */
+  // }
 
-      
-}// End of ClientThread
+
+}
